6c87ff20624d7a216916c2b52f89868d7f9f4c78
Sergio Paredes <sergio.paredes@est.fib.upc.edu>
2018-11-22 20:24:51 +0100

    Add sample car on scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        print("hola");
+        rb.AddForce(movement * speed);
+    }
+
+}
