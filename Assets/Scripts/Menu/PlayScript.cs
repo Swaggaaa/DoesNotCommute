bc0a8a241197c9e086485b7befb6e2a85ded1aff
Swaggaaa <seribanyez@gmail.com>
2018-11-24 21:12:54 +0100

    Updated Menu Scene play

--- Assets/Scripts/Menu/PlayScript.cs
+++ Assets/Scripts/Menu/PlayScript.cs
@@ -7,8 +7,14 @@ using UnityEngine.UI;
 public class PlayScript : MonoBehaviour {
 
 
+    private bool playPressed;
+    public Camera mainCamera;
+    private Vector3 tvPosition;
+
 	// Use this for initialization
 	void Start () {
+        playPressed = false;
+        tvPosition = transform.position;
         Button button = gameObject.GetComponent<Button>();
         button.onClick.AddListener(playListener);
 	}
@@ -20,6 +26,7 @@ public class PlayScript : MonoBehaviour {
 
     IEnumerator playGame()
     {
+        playPressed = true;
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene("SampleScene");
     }
@@ -27,5 +34,15 @@ public class PlayScript : MonoBehaviour {
     void playListener()
     {
         StartCoroutine(playGame());
+    }
+
+    void LateUpdate()
+    {
+        if (playPressed)
+        {
+            // Hide the canvas containing the button
+            transform.parent.transform.position = new Vector3(100f, 100f, 100f);
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, tvPosition, Time.deltaTime);
+        }
     }
 }
