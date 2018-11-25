c1739037da08af2e03924629873cf124baeba552
Sergio Paredes <sergio.paredes@est.fib.upc.edu>
2018-11-25 01:50:04 +0100

    Improve car movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,30 @@ public class PlayerController : MonoBehaviour
     public float rotation;
 
     private Rigidbody rb;
+    private const float multiplier = 10;
+    private const float maxSpeed = 12;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        speed *= rb.mass * multiplier;
+        rotation *= rb.mass * multiplier;
+        rb.maxAngularVelocity = 1f;
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float turningMovement = Input.GetAxis("Horizontal");
+        float linearMovement = Input.GetAxis("Vertical");
 
-        rb.AddForce(transform.forward * moveVertical * speed);
-        rb.AddTorque(transform.up * moveHorizontal * rotation);
+        if (rb.velocity.z < maxSpeed)
+            rb.AddForce(transform.forward * linearMovement * speed);
+
+
+        Vector3 torqueForce = transform.up * turningMovement * rotation;
+        if (linearMovement < 0) torqueForce = -torqueForce;
+        rb.AddTorque(torqueForce);
+
     }
 
 }
