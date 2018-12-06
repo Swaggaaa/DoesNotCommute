cefe9da2d13304611f200bf2ea51bc91e5851a74
Swaggaaa <seribanyez@gmail.com>
2018-12-06 02:25:34 +0100

    Layers and coin pick up

--- Assets/Scripts/Coin/Coin.cs
+++ Assets/Scripts/Coin/Coin.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour {
 
+    private bool disappearing = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +14,29 @@ public class Coin : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Rotate(new Vector3(0f, 35 * Time.deltaTime, 0f));
+
+        if (disappearing)
+        {
+            MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+
+            foreach (var renderer in renderers)
+            {
+                Color color = renderer.material.color;
+                renderer.material.color = new Color(color.r, color.g, color.b, Mathf.Lerp(color.a, 0.0f, 2.0f * Time.deltaTime));
+            }
+        }
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GlobalValues.TimeLeft += 10.0f;
+        GetComponent<Collider>().enabled = false;
+        GetComponent<Animator>().Play("PickUpCoin");
+        disappearing = true;
+    }
+
+    public void Destroy()
+    {
+       Destroy(gameObject);
+    }
 }
