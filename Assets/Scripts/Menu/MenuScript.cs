9235b3223505b5df6b0aab9204381b1995ab2a34
Sergio Paredes <sergio.paredes@est.fib.upc.edu>
2018-12-13 12:19:27 +0100

    Add stage selector

--- Assets/Scripts/Menu/MenuScript.cs
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class MenuScript : MonoBehaviour {
 
     public GameObject settingsPanel;
+    public GameObject scenesPanel;
 
 	// Use this for initialization
 	void Start () {
         settingsPanel.transform.position = new Vector3(100f, 100f, 100f);
+        scenesPanel.transform.position = new Vector3(100f, 100f, 100f);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Menu/PlayStageScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayStageScript : MonoBehaviour {
+
+
+    private bool playPressed;
+    public Camera mainCamera;
+    public string stage;
+    private Vector3 tvPosition;
+
+	// Use this for initialization
+	void Start () {
+        playPressed = false;
+        tvPosition = transform.position;
+        Button button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(PlayListener);
+	}
+
+    IEnumerator PlayGame()
+    {
+        playPressed = true;
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(stage);
+    }
+
+    void PlayListener()
+    {
+        StartCoroutine(PlayGame());
+    }
+
+    void LateUpdate()
+    {
+        if (playPressed)
+        {
+            // Hide the canvas containing the button
+            transform.parent.transform.position = new Vector3(100f, 100f, 100f);
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, tvPosition, Time.deltaTime);
+        }
+    }
+}
