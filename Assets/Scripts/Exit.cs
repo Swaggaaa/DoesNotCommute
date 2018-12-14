909e8de00df0dece4628b65d60355b152e339ff5
Swaggaaa <seribanyez@gmail.com>
2018-12-14 21:05:59 +0100

    Fixed pause menu

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,12 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour {
     
 	public void ExitGame () {
-        #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-        #else
-            Application.Quit();
-        #endif
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuScene");
     }
 }
